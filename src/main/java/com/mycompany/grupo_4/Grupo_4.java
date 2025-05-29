28707939d7b2087572ebaa0a9ae944480ff9ce42
Ashley1905 <astenorio@latinoamericano.edu.ec>
2025-05-28 22:39:27 -0500

    Update Grupo_4.java

--- src/main/java/com/mycompany/grupo_4/Grupo_4.java
+++ src/main/java/com/mycompany/grupo_4/Grupo_4.java
@@ -9,8 +9,68 @@ package com.mycompany.grupo_4;
  * @author DAVID
  */
 public class Grupo_4 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int opcion = -1;
+
+        while (opcion != 0) {
+            System.out.println("\n1. Calcular factorial");
+            System.out.println("2. Tabla de multiplicar");
+            System.out.println("3. Números amigos");
+            System.out.println("0. Salir");
+            System.out.print("Opción: ");
+            opcion = sc.nextInt();
+
+        if (opcion == 1) {
+            System.out.print("Número: ");
+            int n = sc.nextInt();
+            int resultado = 1;
+            for (int i = 1; i <= n; i++) {
+            resultado *= i;
+        }
+    System.out.println("Factorial: " + resultado);
+    }
+            else if (opcion == 2) {
+            System.out.print("Número: ");
+            int n = sc.nextInt();
+            for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+            else if (opcion == 3) {
+            System.out.print("Primer número: ");
+            int a = sc.nextInt();
+            System.out.print("Segundo número: ");
+            int b = sc.nextInt();
+
+            int suma1 = 0;
+            for (int i = 1; i < a; i++) {
+            if (a % i == 0) {
+            suma1 += i;
+        }
+    }
+            int suma2 = 0;
+            for (int i = 1; i < b; i++) {
+            if (b % i == 0) {
+                suma2 += i;
+        }
+    }
+
+            if (suma1 == b && suma2 == a) {
+                System.out.println("Son amigos.");
+            } else {
+                System.out.println("No son amigos.");
+        }
+    }
+            else if (opcion == 0) {
+                System.out.println("Fin del programa.");
+        }
+
+            else {
+                System.out.println("Opción inválida.");
+            }
+        }
     }
 }
